b7d657e9d458fc214456534d01ee17ea651817d5
Nikolas Gomes de Sá <nikolas_sa@outlook.com>
2023-08-23 17:50:34 -0300

    Notebook AWS

--- atividade03/src/json_loader.py
+++
@@ -1,8 +0,0 @@
-import json
-    
-class JSONLoader:
-    @staticmethod
-    def load_json(file_path, encoding='utf-8'):
-        with open(file_path, 'r', encoding=encoding) as json_file:
-            data = json.load(json_file)
-        return data
--- atividade03/src/write.py
+++
@@ -1,7 +0,0 @@
-import os
-
-class DataWriter:
-    def write_parquet_data(self, df, output_directory, file_name):
-        if not os.path.exists(output_directory):
-            os.makedirs(output_directory)
-        df.to_parquet(os.path.join(output_directory, file_name), engine='fastparquet')
